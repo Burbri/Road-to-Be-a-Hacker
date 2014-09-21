3d586a89b166ed8c015fca4778463731988b0010
Bohyun Kim <bohyunk@uci.edu>
2014-09-21 16:12:15 -0400

    fix git by adding changeto

--- GRE_Practice.py
+++ GRE_Practice.py
@@ -1,3 +1,10 @@
 __author__ = 'Boh'
 
 #09/17/2014
+
+
+class Card:
+    def __init__(self, word, definition):
+        """creates a card with a word and definition"""
+        self.word = word
+        self.definition = definition
